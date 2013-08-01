529987509d9f4f09a231b74280a6fd78c4bf8639
Ben Scott <ben@belfryimages.com.au>
2013-08-01 23:51:07 +1000

    trivial app

--- postsharp-fundamentals/postsharp-tutorial/src/LookMaIDoneWroteAConsoleApp/Program.cs
+++ postsharp-fundamentals/postsharp-tutorial/src/LookMaIDoneWroteAConsoleApp/Program.cs
@@ -10,6 +10,20 @@ namespace LookMaIDoneWroteAConsoleApp
     {
         static void Main(string[] args)
         {
+            SayHello();
+            SayGoodbye();
+
+            Console.ReadKey();
+        }
+
+        private static void SayGoodbye()
+        {
+            Console.WriteLine("Goodbye");
+        }
+
+        private static void SayHello()
+        {
+            Console.WriteLine("Hello");
         }
     }
 }
