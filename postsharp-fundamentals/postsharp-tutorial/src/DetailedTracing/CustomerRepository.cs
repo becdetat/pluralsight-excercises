fc2ee5f6105b3d624da12320c71c43304c6d46ce
Ben Scott <ben@belfryimages.com.au>
2013-08-02 00:31:07 +1000

    add logging

--- postsharp-fundamentals/postsharp-tutorial/src/DetailedTracing/CustomerRepository.cs
+++ postsharp-fundamentals/postsharp-tutorial/src/DetailedTracing/CustomerRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using PostSharp.Patterns.Diagnostics;
+using PostSharp.Extensibility;
 
 namespace DetailedTracing
 {
@@ -7,11 +9,13 @@ namespace DetailedTracing
     {
         private static IList<Customer> _db = new List<Customer>();
 
+        [Log]
         public void SaveCustomer(string firstName, string lastName, string street, string city)
         {
             _db.Add(new Customer(firstName, lastName, street, city));
         }
 
+        [Log]
         public Customer[] GetAll()
         {
             return _db.ToArray();
