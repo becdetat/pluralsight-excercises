ac60cf8b8c9f69b6299c4ca18e807c2e527bd651
Ben Scott <ben@belfryimages.com.au>
2013-08-02 00:17:09 +1000

    notifypropertychanged

---
+++ postsharp-fundamentals/postsharp-tutorial/src/inotifypropertychanged/CustomerForEditing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bi.Shared;
+using PostSharp.Patterns.Model;
+
+namespace inotifypropertychanged
+{
+    [NotifyPropertyChanged]
+    class CustomerForEditing
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            // Interesting that this fails when doing the weaving as it calls another class, although the call is static? 
+            //get { return "{0} {1}".FormatWith(FirstName, LastName); }
+            get { return string.Format("{0} {1}", FirstName, LastName); }
+        }
+    }
+}
