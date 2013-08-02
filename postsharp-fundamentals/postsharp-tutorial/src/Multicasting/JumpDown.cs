b1c94ff97012a0827a8957b09875e8c0886bd6d9
Ben Scott <ben@belfryimages.com.au>
2013-08-02 23:38:57 +1000

    stub multicasting app

---
+++ postsharp-fundamentals/postsharp-tutorial/src/Multicasting/JumpDown.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multicasting
+{
+    public class JumpDown : IDoSomething
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Jump down");
+        }
+    }
+}
---
+++ postsharp-fundamentals/postsharp-tutorial/src/Multicasting/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Multicasting
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var somethings = new IDoSomething[]
+                {
+                    new JumpAround(), new JumpAround(), new JumpAround(), new JumpAround(),
+                    new JumpUp(), new JumpUp(),
+                    new JumpDown(),
+                    new Jump(),
+                    new Jump(),
+                    new Jump(),
+                    new Jump(),
+                    new Jump(),
+                    new Jump(),
+                    new Jump(),
+                    new Jump(),
+                    new Jump(),
+                    new Jump(),
+                    new Jump(),
+                    new Jump(),
+                    new Jump(),
+                    new Jump(),
+                    new Jump(),
+                    new Jump(),
+                    new Jump(),
+                };
+
+            foreach (var s in somethings) s.DoSomething();
+            Console.ReadKey();
+        }
+    }
+}
