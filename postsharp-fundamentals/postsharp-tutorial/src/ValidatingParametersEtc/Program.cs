0ccfaf03b025632d3e618a1d5ed4b19c940dfb92
Ben Scott <ben@belfryimages.com.au>
2013-08-03 00:33:37 +1000

    validating parameters used in method calls

---
+++ postsharp-fundamentals/postsharp-tutorial/src/ValidatingParametersEtc/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostSharp.Patterns.Contracts;
+
+namespace ValidatingParametersEtc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NonNullRequired("pass");
+            NonNullRequired(null);  // fail
+            Console.WriteLine();
+        }
+
+        static void NonNullRequired([Required]string s)
+        {}
+    }
+}
