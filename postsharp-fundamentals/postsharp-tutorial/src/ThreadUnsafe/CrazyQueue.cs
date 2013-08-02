c7ae6e64831eac169e22b85dcf2af95d5b86fbdf
Ben Scott <ben@belfryimages.com.au>
2013-08-02 22:39:42 +1000

    thread unsafe crazy queue

---
+++ postsharp-fundamentals/postsharp-tutorial/src/ThreadUnsafe/CrazyQueue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThreadUnsafe
+{
+    public class CrazyQueue
+    {
+        private readonly Queue<int> _queue = new Queue<int>();
+
+        public void Enqueue(int i)
+        {
+            _queue.Enqueue(i);
+        }
+
+        public int? Dequeue()
+        {
+            return _queue.Any() ? _queue.Dequeue() : default(int?);
+        }
+    }
+}
---
+++ postsharp-fundamentals/postsharp-tutorial/src/ThreadUnsafe/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace ThreadUnsafe
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var queue = new CrazyQueue();
+
+            var threads = Environment.ProcessorCount;
+
+            Console.WriteLine("Inconsistencies from queue being shared over {0} threads:", threads);
+
+            Enumerable.Range(0, threads).AsParallel().ForAll(x =>
+                {
+                    var rand = new Random();
+                    for (;;)
+                    {
+                        try
+                        {
+                            int a = rand.Next();
+                            queue.Enqueue(a);
+                            int? b = queue.Dequeue();
+                            if (a != b)
+                                Console.WriteLine("{0}: in {1} out {2}", x, a, b.HasValue ? b.ToString() : "null");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                });
+        }
+    }
+}
