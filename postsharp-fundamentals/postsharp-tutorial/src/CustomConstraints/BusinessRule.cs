69c865be9992fce8663e051ab8276ed91f054d5f
Ben Scott <ben@belfryimages.com.au>
2013-08-02 23:03:00 +1000

    Scalar pattern validation

---
+++ postsharp-fundamentals/postsharp-tutorial/src/CustomConstraints/BusinessRule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomConstraints
+{
+    [BusinessRulePatternValidation]
+    public class PassingBusinessRule
+    {
+        public class Factory
+        {
+        }
+    }
+
+    [BusinessRulePatternValidation]
+    public class FailingBusinessRule
+    {
+    }
+
+    public class UncheckedBusinessRule
+    {
+    }
+}
---
+++ postsharp-fundamentals/postsharp-tutorial/src/CustomConstraints/BusinessRulePatternValidation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostSharp.Constraints;
+using PostSharp.Extensibility;
+
+namespace CustomConstraints
+{
+    [MulticastAttributeUsage(MulticastTargets.Class, Inheritance = MulticastInheritance.Strict)]
+    public class BusinessRulePatternValidation : ScalarConstraint
+    {
+        public override void ValidateCode(object target)
+        {
+            var targetType = (Type) target;
+            if (targetType.GetNestedType("Factory") == null)
+            {
+                Message.Write(
+                    targetType, SeverityType.Warning,
+                    "2001", "The {0} type does not have a nested type named 'Factory'", targetType.Name);
+            }
+        }
+    }
+}
