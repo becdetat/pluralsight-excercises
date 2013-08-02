96b3812a27019b0b6b272f753297fade0f109fd7
Ben Scott <ben@belfryimages.com.au>
2013-08-03 00:22:39 +1000

    event interception

---
+++ postsharp-fundamentals/postsharp-tutorial/src/InterceptEvent/LoggingEventInterceptionAspect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostSharp.Aspects;
+
+namespace InterceptEvent
+{
+    [Serializable]
+    public class LoggingEventInterceptionAspect : EventInterceptionAspect
+    {
+        public override void OnAddHandler(EventInterceptionArgs args)
+        {
+            base.OnAddHandler(args);
+            Console.WriteLine("handler added");
+        }
+        public override void OnRemoveHandler(EventInterceptionArgs args)
+        {
+            base.OnRemoveHandler(args);
+            Console.WriteLine("handler removed");
+        }
+
+        public override void OnInvokeHandler(EventInterceptionArgs args)
+        {
+            base.OnInvokeHandler(args);
+            Console.WriteLine("handler invoked");
+        }
+    }
+}
---
+++ postsharp-fundamentals/postsharp-tutorial/src/InterceptEvent/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterceptEvent
+{
+    public class Program
+    {
+        [LoggingEventInterceptionAspect]
+        public event EventHandler MyEvent; 
+
+        public void RaiseMyEvent()
+        {
+            if (MyEvent != null) MyEvent(this, EventArgs.Empty);
+        }
+
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            EventHandler handler = (sender, eventArgs) => Console.WriteLine("MyEvent was raised");
+
+            program.MyEvent += handler;
+            
+            program.RaiseMyEvent();
+
+            program.MyEvent -= handler;
+
+            Console.ReadKey();
+        }
+    }
+}
